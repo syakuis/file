3cac2babaf66a09af1045ddc3f082f3009ad92b7
syaku <syaku@naver.com>
2021-11-11 01:02:06 +0900

    ,,,,

---
+++ file-client/src/main/java/io/github/syakuis/file/support/HttpImageFileLoader.java
@@ -0,0 +1,31 @@
+package io.github.syakuis.file.support;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * @author Seok Kyun. Choi.
+ * @since 2021-11-11
+ */
+@Slf4j
+public class HttpImageFileLoader {
+    public static void save(String uri, String target) {
+        try {
+            URL url = new URL(uri);
+            URLConnection urlConnection = url.openConnection();
+            InputStream inputStream = urlConnection.getInputStream();
+            FileOutputStream fileOutputStream = new FileOutputStream(target);
+            byte[] buffer = new byte[1024];
+            int readBytes;
+            while ((readBytes = inputStream.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, readBytes);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+}
--- file-client/src/test/java/io/github/syakuis/HttpFileLoader.java
+++ file-client/src/test/java/io/github/syakuis/HttpFileLoader.java
@@ -1,29 +1,36 @@
 package io.github.syakuis;
 
+import io.github.syakuis.file.support.HttpImageFileLoader;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
+import java.time.Duration;
 
 /**
  * @author Seok Kyun. Choi.
  * @since 2021-11-09
  */
+@Slf4j
 class HttpFileLoader {
+    private long beforeTime = 0;
     private final String uri = "https://image.news1.kr/system/photos/2021/10/20/5024865/article.jpg/dims/optimize";
+
+
+    @BeforeEach
+    public void init() {
+        this.beforeTime = System.currentTimeMillis();
+    }
+
+    @AfterEach
+    public void exit() {
+        long afterTime = System.currentTimeMillis();
+        log.debug("{} = {} - {}", afterTime - beforeTime, afterTime, beforeTime);
+    }
 
     @Test
     void test() throws Exception {
-        URL url = new URL(uri);
-        URLConnection urlConnection = url.openConnection();
-        InputStream inputStream = urlConnection.getInputStream();
-        FileOutputStream fileOutputStream = new FileOutputStream("./temp/image.jpeg");
-        byte[] buffer = new byte[1024];
-        int readBytes;
-        while ((readBytes = inputStream.read(buffer)) != -1) {
-            fileOutputStream.write(buffer, 0, readBytes);
-        }
+        HttpImageFileLoader.save(uri, "./temp/image.jpeg");
     }
 }
